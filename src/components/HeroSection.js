d365bae230442988081c3aca70b8c8cbceae7c3a
Suyash <suyashsingh2708@gmail.com>
2025-01-26 18:53:36 +0530

    White Screen Animation fixed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -54,7 +54,7 @@ import WhiteScreenAnimation from "./WhiteScreenAnimation";
 const HeroSection = () => {
   return (
     <div className="container mx-auto relative">
-      {/* <WhiteScreenAnimation serviceName="Elixir-M" /> */}
+      <WhiteScreenAnimation serviceName="Elixir-M" />
       <div className="grid grid-cols-1 md:grid-cols-2 min-h-[850px] pt-8">
         {/* Left Section */}
         <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left p-8 md:p-16 space-y-4">
@@ -82,7 +82,7 @@ const HeroSection = () => {
           right: 0,
           zIndex: 10,
           background: "black",
-          marginTop: "-7%", // Adjust as needed
+          // marginTop: "-7%", 
         }}
       >
         <FocusCardsDemo />
